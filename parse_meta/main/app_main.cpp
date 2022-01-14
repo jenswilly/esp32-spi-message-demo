68e5f24254ebdfc22daa578b936ea6282d37c030
Jens Willy Johannsen <jens@jenswilly.dk>
2022-01-14 16:15:59 +0100

    Correct mobilenet categories + work on output

--- parse_meta/main/app_main.cpp
+++ parse_meta/main/app_main.cpp
@@ -2,10 +2,10 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <vector>
 
 #include "decode_raw_mobilenet.h"
 #include "esp32_spi_impl.h"
-
 #include "spi_api.hpp"
 
 #define MAX_DETECTIONS 16
@@ -17,44 +17,78 @@ extern "C" {
    void app_main();
 }
 
-void run_demo(){
-    uint8_t req_success = 0;
+static std::vector<std::string> labels = {
+	"background",
+	"aeroplane",
+	"bicycle",
+	"bird",
+	"boat",
+	"bottle",
+	"bus",
+	"car",
+	"cat",
+	"chair",
+	"cow",
+	"diningtable",
+	"dog",
+	"horse",
+	"motorbike",
+	"person",
+	"pottedplant",
+	"sheep",
+	"sofa",
+	"train",
+	"tvmonitor"
+};
 
-    dai::SpiApi mySpiApi;
-    mySpiApi.set_send_spi_impl(&esp32_send_spi);
-    mySpiApi.set_recv_spi_impl(&esp32_recv_spi);
+void run_demo() {
+	uint8_t req_success = 0;
 
-    while(1) {
-        // ----------------------------------------
-        // basic example of receiving data and metadata from messages.
-        // ----------------------------------------
-        dai::Message received_msg;
-        if(mySpiApi.req_message(&received_msg, METASTREAM)){
-            // example of parsing the raw metadata
-            dai::RawImgDetections det;
-            mySpiApi.parse_metadata(&received_msg.raw_meta, det);
-            for(const auto& det : det.detections){
-                printf("label: %d, xmin: %f, ymin: %f, xmax: %f, ymax: %f\n", det.label, det.xmin, det.ymin, det.xmax, det.ymax);
-            }
+	dai::SpiApi mySpiApi;
+	mySpiApi.set_send_spi_impl(&esp32_send_spi);
+	mySpiApi.set_recv_spi_impl(&esp32_recv_spi);
 
-            // free up resources once you're done with the message.
-            // and pop the message from the queue
-            mySpiApi.free_message(&received_msg);
-            mySpiApi.spi_pop_messages();
-        }
-    }
+	while(1) {
+		// ----------------------------------------
+		// basic example of receiving data and metadata from messages.
+		// ----------------------------------------
+		dai::Message received_msg;
+
+		if(mySpiApi.req_message(&received_msg, METASTREAM)) {
+			// example of parsing the raw metadata
+			dai::RawImgDetections det;
+			mySpiApi.parse_metadata(&received_msg.raw_meta, det);
+
+			if(det.detections.size() == 0 ) {
+				// No detections
+				printf(".\n");
+			} else {
+				printf("\n%d detections\n", det.detections.size());
+				// Iterate individual detections
+				for(const dai::ImgDetection& det : det.detections) {
+					if(det.label <= labels.size())
+						printf("label: %d, name: %s, confidence: %.2f\n", det.label, labels[det.label].c_str(), det.confidence);
+					else
+						printf("label: %d, confidence: %.2f\n", det.label, det.confidence);
+				}
+			}
+
+			// free up resources once you're done with the message.
+			// and pop the message from the queue
+			mySpiApi.free_message(&received_msg);
+			mySpiApi.spi_pop_messages();
+		}
+	}
 }
 
-//Main application
+// Main application
 void app_main()
 {
-
-    // init spi for the esp32
+    // Init spi for the esp32
     init_esp32_spi();
 
     run_demo();
 
-    //Never reached.
+    // Never reached
     deinit_esp32_spi();
-
 }
